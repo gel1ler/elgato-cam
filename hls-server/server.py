7187afba30b686f29096d6df190e16f33440798d
Aleksandr Abanichev <abanichev16@gmail.com>
2025-06-22 18:10:58 +0300

    init

---
+++ hls-server/server.py
@@ -0,0 +1,16 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    os.chdir('../hls')  # корневая папка с HLS файлами
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, CORSRequestHandler)
+    print("Serving HLS on port 8000 with CORS...")
+    httpd.serve_forever()
