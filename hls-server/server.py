bbfba40abc15c57d2c4cab35d64490a3ff5b365e
gel1ler <abanichev16@gmail.com>
2025-07-16 16:03:38 +0300

    Update RTSP URL, enhance ffmpeg settings, and modify HLS stream duration. Remove unused stream file and add cache control to server response.

--- hls-server/server.py
+++ hls-server/server.py
@@ -6,6 +6,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header('Cache-Control', 'no-cache')  # Отключение кэширования
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
